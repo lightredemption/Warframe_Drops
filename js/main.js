322980f6f269185342e1c6e60e6919479367fa2a
lightredemption <lightredemption@hotmail.com>
2016-02-17 19:45:11 -0500

    ayaayayaay

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
 $(document).ready(function() {
-  var parts;
 
-  $.getJSON('data/parts.json', function(response) {
-    parts = response;
-    console.log(parts);
+  $('#partSearch').DataTable({
+    "bLengthChange": false,
+    "responsive": true,
+    "oLanguage": { "sSearch": "" },
+    "bSort": false,
+    "info": false,
+    "paging": false,
+    "pageLength": 8,
+    "ajax": "data/parts.json",
+    "language": {
+        "searchPlaceholder": "Search records"
+    },
+    "columns": [{
+      "data": "type"
+    }, {
+      "data": "name"
+    }, {
+      "data": "part"
+    }, {
+      "data": "mission"
+    }, {
+      "data": "location"
+    }, {
+      "data": "rotation",
+      "bSearchable": false
+    }]
   });
 
 });
